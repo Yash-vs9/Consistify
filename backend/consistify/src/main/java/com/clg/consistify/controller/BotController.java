c543d3158a74086808a29df166aac79b3148c786
Lakshya Varshney <gq0396@myamu.ac.in>
2025-08-10 14:33:27 +0530

    minor changes

--- backend/consistify/src/main/java/com/clg/consistify/controller/BotController.java
+++ backend/consistify/src/main/java/com/clg/consistify/controller/BotController.java
@@ -19,10 +19,6 @@ public class BotController {
     public BotController(ExternalApiService externalApiService) {
         this.externalApiService = externalApiService;
     }
-    @GetMapping("/getSkills")
-    public CompletableFuture<String> skillsNeeded(String name) throws ExecutionException, InterruptedException, JsonProcessingException {
-
-        return externalApiService.getMessage(name);
-    }
+    
 
 }
--- backend/consistify/src/main/java/com/clg/consistify/controller/TaskController.java
+++ backend/consistify/src/main/java/com/clg/consistify/controller/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/task")
@@ -24,7 +25,7 @@ public class TaskController {
         this.taskService = taskService;
     }
     @PostMapping("/create")
-    public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskDTO body) {
+    public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskDTO body) throws ExecutionException, InterruptedException {
         String username=SecurityContextHolder.getContext().getAuthentication().getName();
         TaskResponseDTO createdTask = taskService.createTask(body,username);
         System.out.println(body);
