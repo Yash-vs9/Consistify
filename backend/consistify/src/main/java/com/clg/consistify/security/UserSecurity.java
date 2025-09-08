f0033ebc70779cd01cdb794700c5fb497f6e7565
Yash Vardhan Shukla <lakkyshukla74339@gmail.com>
2025-09-08 16:57:26 +0530

    added endpoint of vercel

--- backend/consistify/src/main/java/com/clg/consistify/security/UserSecurity.java
+++ backend/consistify/src/main/java/com/clg/consistify/security/UserSecurity.java
@@ -73,7 +73,8 @@ public class UserSecurity {
                 "http://localhost:5173",
                 "http://localhost:3000",
                 "http://localhost:4200",
-                "https://consistify-iota.vercel.app"
+                "https://consistify-iota.vercel.app",
+                "https://consistify2.vercel.app"
         )); // React dev server
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
